2598616b13e1668ebcc867cc7c8419640eed3d15
stungph115 <82810693+stungph115@users.noreply.github.com>
2024-07-15 10:18:57 +0200

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 export const env = {
     URL: 'http://localhost:3050/',
     URL_SOCKET: 'http://localhost:3050/',
-    PUBLIC_KEY_STRIPE: 'pk_test_51OUmh2BBo1z4BCbnLKYaQEinn027HusxiLQ5WcsIAIpFEeNw9rvspBLx1WxIeQuSPuNWDEW4DMtvAEOTBzAomwFG00VvxzFrKJ',
+    PUBLIC_KEY_STRIPE: '',
 }
